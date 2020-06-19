96631d032afcb1814657128bfa0b5803005f6667
Alex Wood <awood45@gmail.com>
2020-06-19 13:25:15 -0700

    Expand EFS File Comment

--- python3.8/cookiecutter-aws-sam-efs-python/{{cookiecutter.project_name}}/hello_efs/app.py
+++ python3.8/cookiecutter-aws-sam-efs-python/{{cookiecutter.project_name}}/hello_efs/app.py
@@ -2,7 +2,8 @@ import json
 from pathlib import Path
 
 # You can reference EFS files by including your local mount path, and then
-# treat them like any other file.
+# treat them like any other file. Local invokes may not work with this, however,
+# as the file/folders may not be present in the container.
 FILE = Path("/mnt/lambda/file")
 
 def lambda_handler(event, context):
