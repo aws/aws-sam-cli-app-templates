a36cd1e5c34715222f402d8830eeda86651046d5
Sam Liu <xinhol@amazon.com>
2020-12-04 14:50:08 -0800

    Remove unnecessary comment

--- tests/integration/build_invoke/test_build_invoke_dotnetcore2_1.py
+++ tests/integration/build_invoke/test_build_invoke_dotnetcore2_1.py
@@ -2,17 +2,12 @@ import pytest
 
 from tests.integration.base import Base
 
-# sam build is likely to fail in the first try because dotnet.lambda cannot be used immediately after installation
-# rerun can make it work
 
-
-@pytest.mark.flaky(reruns=2)
 class BuildInvoke_dotnetcore2_1_cookiecutter_aws_sam_hello_dotnet(Base.BuildInvokeBase):
     use_container = False
     directory = "dotnetcore2.1/cookiecutter-aws-sam-hello-dotnet"
 
 
-@pytest.mark.flaky(reruns=2)
 class BuildInvoke_dotnetcore2_1_cookiecutter_aws_sam_hello_step_functions_sample_app(Base.BuildInvokeBase):
     use_container = False
     directory = "dotnetcore2.1/cookiecutter-aws-sam-hello-step-functions-sample-app"
