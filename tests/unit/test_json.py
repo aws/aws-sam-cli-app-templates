a588297d35d45177c7a90620a1db4bff871d412a
Sam Liu <xinhol@amazon.com>
2020-12-08 10:26:56 -0800

    fix: Fix unmatch template directories (against manifest.json) and add unit test

--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -1,16 +1,26 @@
 import json
 import os
 import unittest
+from pathlib import Path
 
 from tests import REPO_ROOT
+
 
 class TestJson(unittest.TestCase):
     def setUp(self):
         self.manifest_path = os.path.join(REPO_ROOT, "manifest.json")
 
-    def test_manifest_valid_json(self):
-        json_body = None
+    def _load_manifest(self):
         with open(self.manifest_path) as f:
-            body = f.read()
-            json_body = json.loads(body)
+            return json.load(f)
+
+    def test_manifest_valid_json(self):
+        json_body = self._load_manifest()
         self.assertIsNotNone(json_body)
+
+    def test_manifest_template_exist(self):
+        json_body = self._load_manifest()
+        for runtime, templates in json_body.items():
+            for template in templates:
+                directory = template["directory"]
+                self.assertTrue(Path(directory).exists(), f"{directory} does not exist")
