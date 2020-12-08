ee4931941d32b6d3ee1da7f08fdef00ccc940b12
Sam Liu <xinhol@amazon.com>
2020-12-07 16:47:31 -0800

    fix: fix the missing paths/files in python eventbridge schema templates

---
+++ python3.6/cookiecutter-aws-sam-eventbridge-schema-app-python/{{cookiecutter.project_name}}/{{cookiecutter.function_name}}/{{cookiecutter.AWS_Schema_root}}/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from {{cookiecutter.AWS_Schema_root}}.marshaller import Marshaller
+from {{cookiecutter.AWS_Schema_root}}.aws_event import AWSEvent
+from {{cookiecutter.AWS_Schema_root}}.ec2_instance_state_change_notification import {{cookiecutter.AWS_Schema_name}}
