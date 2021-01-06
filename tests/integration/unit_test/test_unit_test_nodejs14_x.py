6694533575e40e0c06f8344d9745f6c2e6b649e8
Raymond Wang <14915548+wchengru@users.noreply.github.com>
2021-01-06 10:50:58 -0800

    Add unit/integ tests

---
+++ tests/integration/unit_test/test_unit_test_nodejs14_x.py
@@ -0,0 +1,39 @@
+from tests.integration.unit_test.unit_test_base import UnitTestBase
+
+
+class UnitTest_nodejs14_x_cookiecutter_aws_sam_hello_nodejs(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-aws-sam-hello-nodejs"
+    code_directories = ["hello-world"]
+
+
+class UnitTest_nodejs14_x_cookiecutter_aws_sam_step_functions_sample_app(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-aws-sam-step-functions-sample-app"
+    code_directories = [
+        "functions/stock-buyer",
+        "functions/stock-checker",
+        "functions/stock-seller",
+    ]
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_from_scratch(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-from-scratch"
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_cloudwatch_events(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-cloudwatch-events"
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_s3(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-s3"
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_sns(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-sns"
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_sqs(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-sqs"
+
+
+class UnitTest_nodejs14_x_cookiecutter_quick_start_web(UnitTestBase.NodejsUnitTestBase):
+    directory = "nodejs14.x/cookiecutter-quick-start-web"
