27dce06faabe6a05ba791cc9ce2f28f02fe84700
satmihir <mihsathe@gmail.com>
2021-03-18 12:04:37 -0700

    Add machine learning inference templates for Python (#86)
    * Add machine learning inference templates for Python
    
    Co-authored-by: Mihir Sathe <satmihir@amazon.com>
    Co-authored-by: _sam <3804518+aahung@users.noreply.github.com>

--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -24,29 +24,3 @@ class TestJson(unittest.TestCase):
             for template in templates:
                 directory = template["directory"]
                 self.assertTrue(Path(directory).exists(), f"{directory} does not exist")
-
-    def test_cookiecutter_json(self):
-        json_body = self._load_manifest()
-        for runtime, templates in json_body.items():
-            for template in templates:
-                cookiecutter_json_path = Path(
-                    template["directory"], "cookiecutter.json"
-                )
-                with open(cookiecutter_json_path) as f:
-                    cookiecutter_json_body = json.load(f)
-                    self.assertIn(
-                        "project_name",
-                        cookiecutter_json_body,
-                        f"{cookiecutter_json_path} does not have project_name",
-                    )
-                    self.assertIn(
-                        "runtime",
-                        cookiecutter_json_body,
-                        f"{cookiecutter_json_path} does not have runtime",
-                    )
-                    actual_runtime = cookiecutter_json_body["runtime"]
-                    self.assertIn(
-                        runtime,
-                        {runtime, f"amazon/{actual_runtime}-base"},
-                        f"{cookiecutter_json_path} has incorrect runtime {actual_runtime}",
-                    )
