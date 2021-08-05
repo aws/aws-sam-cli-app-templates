cc3fc5f564171e74dce0dcac4a17e831d3a79dd6
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2021-08-05 16:05:18 +1000

    Minimal example for the S3 Rust template

--- rust-1.x/cookiecutter-aws-sam-s3-rust/src/main.rs
+++ rust-1.x/cookiecutter-aws-sam-s3-rust/src/main.rs
@@ -1,15 +1,49 @@
-use lambda_runtime::{handler_fn, Context, Error};
-use serde_json::{json, Value};
+extern crate jemallocator;
+
+#[global_allocator]
+static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use bytes::Bytes;
+use lambda_runtime::{ handler_fn, Context, Error };
+use serde_json::{ json, Value };
+
+use aws_sdk_s3 as s3;
+use s3::Region;
+
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::SubscriberBuilder;
+
+// Change these to your bucket, key and region
+const BUCKET: &str = "samlocal";
+const KEY: &str = "testobject.txt";
+const REGION: &str = "ap-southeast-2";
+
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = handler_fn(func);
-    lambda_runtime::run(func).await?;
+    lambda_runtime::run(handler_fn(read_s3_object)).await?;
     Ok(())
 }
 
-async fn func(event: Value, _: Context) -> Result<Value, Error> {
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+async fn read_s3_object(_event: Value, _ctx: Context) -> Result<Value, Error> {
+    let s3_object = stream_s3_object().await?;
+    dbg!(&s3_object);
+    Ok(json!({ "message": &*s3_object }))
+}
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+/// Fetches S3 object
+async fn stream_s3_object() -> Result<Bytes, Error> {
+    SubscriberBuilder::default()
+        .with_env_filter("info")
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+    let conf = s3::Config::builder()
+        .region(Region::new(REGION))
+        .build();
+    let client = s3::Client::from_conf(conf);
+
+    let resp = client.get_object().bucket(BUCKET).key(KEY).send().await?;
+    let data = resp.body.collect().await?;
+
+    return Ok(data.into_bytes());
 }
