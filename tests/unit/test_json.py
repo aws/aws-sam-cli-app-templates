3d0e6bf353459605f0d18a55c769d2a6bbc0232e
Jonathan Ifegunni <jonife@amazon.com>
2021-12-09 16:12:54 -0600

    test updated

--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -23,6 +23,6 @@ class TestJson(unittest.TestCase):
         for runtime, templates in json_body.items():
             for template in templates:
                 directory = template["directory"]
-                use_case_name = template["useCaseName"]
+                use_case_name = template.get("useCaseName")
                 self.assertTrue(Path(directory).exists(), f"{directory} does not exist")
-                self.assertTrue(bool(use_case_name != None), "Template property 'useCaseName' was not specified")
+                self.assertIsNotNone(use_case_name, "Template property 'useCaseName' was not specified")
