0eaad86914ab02fe8e01ae8a28722476962940b5
Nicolas Moutschen <nicolas.moutschen@gmail.com>
2022-02-25 06:55:22 +0100

    chore(rust): rename folder to 'rust (provided.al2)'

--- tests/integration/unit_test/test_unit_test_rust.py
+++ tests/integration/unit_test/test_unit_test_rust.py
@@ -2,4 +2,4 @@ from tests.integration.unit_test.unit_test_base import UnitTestBase
 
 
 class UnitTest_rust_cookiecutter_aws_sam_hello_rust(UnitTestBase.RustUnitTestBase):
-    directory = "rust(provided.al2)/cookiecutter-aws-sam-hello-rust"
+    directory = "rust (provided.al2)/cookiecutter-aws-sam-hello-rust"
