224526de2ab41a37e2d48d8a9b78e0fa6565f9a4
Iain Hunter <iain@inawisdom.com>
2022-09-04 14:29:27 +0100

    add pytest fixture and improve test

--- python3.9/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/tests/integration/test_api_gateway.py
+++ python3.9/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/tests/integration/test_api_gateway.py
@@ -12,7 +12,7 @@ Make sure env variable AWS_SAM_STACK_NAME exists with the name of the stack we a
 class TestApiGateway:
 
     @pytest.fixture()
-    def api_gateway_url(self, set_env):
+    def api_gateway_url(self):
         """ Get the API Gateway URL from Cloudformation Stack outputs """
         stack_name = os.environ.get("AWS_SAM_STACK_NAME")
         client = boto3.client("cloudformation")
