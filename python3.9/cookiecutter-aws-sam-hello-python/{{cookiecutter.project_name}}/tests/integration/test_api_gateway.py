2a34f0843ad6cbb52b0783888740f84b1f5a349a
Iain Hunter <iain@inawisdom.com>
2022-09-04 14:53:58 +0100

    tidy up tests

--- python3.9/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/tests/integration/test_api_gateway.py
+++ python3.9/cookiecutter-aws-sam-hello-python/{{cookiecutter.project_name}}/tests/integration/test_api_gateway.py
@@ -15,13 +15,17 @@ class TestApiGateway:
     def api_gateway_url(self):
         """ Get the API Gateway URL from Cloudformation Stack outputs """
         stack_name = os.environ.get("AWS_SAM_STACK_NAME")
+
+        if stack_name is None:
+            raise ValueError('Please set the AWS_SAM_STACK_NAME environment variable to the name of your stack')
+
         client = boto3.client("cloudformation")
 
         try:
             response = client.describe_stacks(StackName=stack_name)
         except Exception as e:
             raise Exception(
-                f"Cannot find stack {stack_name} \n" f'Please make sure stack with the name "{stack_name}" exists'
+                f"Cannot find stack {stack_name} \n" f'Please make sure a stack with the name "{stack_name}" exists'
             ) from e
 
         stacks = response["Stacks"]
