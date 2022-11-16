6c6b6650d194fe70a289c9829a3ce0f825541954
Mehmet Nuri Deveci <5735811+mndeveci@users.noreply.github.com>
2022-11-15 19:23:06 -0800

    feat: nodejs18 (#309)
    * feat: nodejs18 (#118)
    
    * feat: nodejs18
    
    * Resolve comments
    
    * Copy ESM file in Dockerfile
    
    * Remove unreachable return statement
    
    * Add tests
    
    * Comment out build_invoke node18 tests
    
    * Update buildspec.yml
    
    * Update buildspec-unittest-nodejs-16.x.yml
    
    * Update buildspec-unittest-nodejs-18.x.yml
    
    * update build image version
    
    * Update buildspec-unittest-nodejs-16.x.yml
    
    * Update buildspec-unittest-nodejs-18.x.yml
    
    * Update buildspec.yml
    
    * Run dotnet tests sequentially
    
    * Run dotnet tests sequentially
    
    Co-authored-by: Andrea Culot <95755271+andclt@users.noreply.github.com>

---
+++ nodejs18.x-image/cookiecutter-aws-sam-hello-nodejs-lambda-image/{{cookiecutter.project_name}}/hello-world/app.mjs
@@ -0,0 +1,28 @@
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+
+export const lambdaHandler = async (event, context) => {
+    try {
+        return {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'hello world',
+            })
+        }
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+
+    return response
+};
