90388fae1931a7f8c310d4a21e768c8061c35598
Alexander Melnyk <amelnyk@amazon.com>
2023-02-02 17:32:38 +0100

    add unit tests for powertools

--- tests/integration/unit_test/test_unit_test_java8.py
+++ tests/integration/unit_test/test_unit_test_java8.py
@@ -12,6 +12,13 @@ class UnitTest_java8_cookiecutter_aws_sam_hello_java_maven(UnitTestBase.JavaUnit
     directory = "java8/hello-maven"
     code_directories = ["HelloWorldFunction"]
 
+class UnitTest_java8_cookiecutter_aws_sam_hello_java_powertools_maven(UnitTestBase.JavaUnitTestMavenBase):
+    directory = "java8/hello-powertools-maven"
+    code_directories = ["HelloWorldFunction"]
+
+class UnitTest_java8_cookiecutter_aws_sam_hello_java_powertools_gradle(UnitTestBase.JavaUnitTestGradleBase):
+    directory = "java8/hello-powertools-gradle"
+    code_directories = ["HelloWorldFunction"]
 
 class UnitTest_java8_cookiecutter_aws_sam_eventbridge_hello_java_gradle(UnitTestBase.JavaUnitTestGradleBase):
     directory = "java8/event-bridge-gradle"
--- tests/integration/unit_test/test_unit_test_java8_al2.py
+++ tests/integration/unit_test/test_unit_test_java8_al2.py
@@ -12,6 +12,13 @@ class UnitTest_java8_al2_cookiecutter_aws_sam_hello_java_maven(UnitTestBase.Java
     directory = "java8.al2/hello-maven"
     code_directories = ["HelloWorldFunction"]
 
+class UnitTest_java8_al2_cookiecutter_aws_sam_hello_java_powertools_maven(UnitTestBase.JavaUnitTestMavenBase):
+    directory = "java8.al2/hello-powertools-maven"
+    code_directories = ["HelloWorldFunction"]
+
+class UnitTest_java8_al2_cookiecutter_aws_sam_hello_java_powertools_gradle(UnitTestBase.JavaUnitTestGradleBase):
+    directory = "java8.al2/hello-powertools-gradle"
+    code_directories = ["HelloWorldFunction"]
 
 class UnitTest_java8_al2_cookiecutter_aws_sam_eventbridge_hello_java_gradle(UnitTestBase.JavaUnitTestGradleBase):
     directory = "java8.al2/event-bridge-gradle"
