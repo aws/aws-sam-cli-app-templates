76fa91e165d66d4b413611568568fb05cc1e1137
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2023-02-22 12:42:33 -0800

    Rust cargo lambda (#341)
    * Update rust template to use Rust BuildMethod
    
    * Update rust template to use rustcargolambda BuildMethod
    
    * Update rust tests
    
    * Remove Rust unit test from buildspec
    
    * debug buildspec-rust.yml
    
    * update rust buildspec
    
    * Update dir struct for rust hello
    
    * rename build method

---
+++ tests/integration/build_invoke/test_build_invoke_rust.py
@@ -0,0 +1,17 @@
+from unittest import skip
+from tests.integration.build_invoke.build_invoke_base import BuildInvokeBase
+
+"""
+sam build does not support to build go 1.x templates using container, 
+here we put them in a separate file and use a dedicate codebuild project with go 1.x runtime to build them.
+
+For each template, it will test the following sam commands:
+1. sam init
+2. sam build --use-container (if self.use_container is False, --use-container will be omitted)
+3. (if there are event jsons), for each event json, check `sam local invoke` response is a valid json
+"""
+
+class BuildInvoke_provided_rust_cookiecutter_aws_sam_hello(BuildInvokeBase.BuildInvokeBase):
+    use_container = False
+    directory = "al2/rust/hello"
+    beta_features = True
