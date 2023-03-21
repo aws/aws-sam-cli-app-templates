4c545a9aaed946dc21e4358175e7f3a4e7a9fa18
Sean O Brien <sean.obrien.2016@mumail.ie>
2023-03-21 07:34:14 +0000

    feat: add python3.10. (#125)
    * Add python3.10.
    
    * Update buildspec.yml
    
    Co-authored-by: Lucas <12496191+lucashuy@users.noreply.github.com>
    
    * Update buildspecs/buildspec-unittest-python3.10.yml
    
    Co-authored-by: Lucas <12496191+lucashuy@users.noreply.github.com>
    
    * Update tests/integration/unit_test/unit_test_base.py
    
    Co-authored-by: Lucas <12496191+lucashuy@users.noreply.github.com>
    
    * Add ZIP template support for python3.10
    
    * Use shorter directory names for python3.10.
    
    ---------
    
    Co-authored-by: Lucas <12496191+lucashuy@users.noreply.github.com>

---
+++ tests/integration/unit_test/test_unit_test_python3_10.py
@@ -0,0 +1,52 @@
+from tests.integration.unit_test.unit_test_base import UnitTestBase
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_hello_python(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-hello-python"
+    code_directories = ["hello_world"]
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_eventBridge_python(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-eventBridge-python"
+    code_directories = ["hello_world_function"]
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_eventbridge_schema_app_python(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-eventbridge-schema-app-python"
+    code_directories = ["hello_world_function"]
+
+    def _test_unit_tests(self, code_directory: str):
+        self.skipTest("eventbridge schema app requires credential to pull missing files, skip")
+        pass
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_step_functions_sample_app(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-step-functions-sample-app"
+    code_directories = [
+        "functions/stock_buyer",
+        "functions/stock_checker",
+        "functions/stock_seller",
+    ]
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_efs_python(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-efs-python"
+    code_directories = ["hello_efs"]
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_quick_start_web_with_connectors(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-quick-start-web-with-connectors"
+    code_directories = [
+        "src/handlers/get_all_items", 
+        "src/handlers/get_by_id", 
+        "src/handlers/put_item"
+    ]
+
+
+class UnitTest_python3_10_cookiecutter_aws_sam_step_functions_with_connectors(UnitTestBase.Python310UnitTestBase):
+    directory = "python3.10/cookiecutter-aws-sam-step-functions-with-connectors"
+    code_directories = [
+        "functions/stock_buyer",
+        "functions/stock_checker",
+        "functions/stock_seller",
+    ]
