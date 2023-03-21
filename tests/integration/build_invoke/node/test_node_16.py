bcc98813aac6aa52424a33a006540eb0477d2386
Lucas <12496191+lucashuy@users.noreply.github.com>
2023-03-21 14:19:10 -0700

    feat: Moved tests to Github Actions (#358)
    * Added install action
    
    * Added initial workflow file
    
    * Trigger actions
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * trigger
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow
    
    * Update workflow

---
+++ tests/integration/build_invoke/node/test_node_16.py
@@ -0,0 +1,46 @@
+from tests.integration.build_invoke.build_invoke_base import BuildInvokeBase
+
+"""
+For each template, it will test the following sam commands:
+1. sam init
+2. sam build --use-container (if self.use_container is False, --use-container will be omitted)
+3. (if there are event jsons), for each event json, check `sam local invoke` response is a valid json
+"""
+
+class BuildInvoke_nodejs16_x_cookiecutter_aws_sam_hello_nodejs(BuildInvokeBase.SimpleHelloWorldBuildInvokeBase):
+    directory = "nodejs16.x/hello"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_aws_sam_step_functions_sample_app(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/step-func"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_from_scratch(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/scratch"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_cloudwatch_events(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/cw-event"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_s3(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/s3"
+
+    # if we want to check response json, we need to setup bucket for testing
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_sns(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/sns"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_sqs(BuildInvokeBase.BuildInvokeBase):
+    directory = "nodejs16.x/sqs"
+
+
+class BuildInvoke_nodejs16_x_cookiecutter_quick_start_web(BuildInvokeBase.QuickStartWebBuildInvokeBase):
+    directory = "nodejs16.x/web"
+
+class BuildInvoke_image_nodejs16_x_cookiecutter_aws_sam_hello_nodejs_lambda_image(
+    BuildInvokeBase.SimpleHelloWorldBuildInvokeBase
+):
+    directory = "nodejs16.x/hello-img"
