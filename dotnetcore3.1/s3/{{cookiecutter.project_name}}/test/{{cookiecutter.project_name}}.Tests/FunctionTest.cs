498ab4a7dd4aa6a164c0cfa7b3e997e427a1e21c
Duy Tran <duytran.developer@gmail.com>
2023-03-31 05:22:05 +0800

    Mock S3 response for unit test (#370)
    Co-authored-by: Lucas <12496191+lucashuy@users.noreply.github.com>

--- dotnetcore3.1/s3/{{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}.Tests/FunctionTest.cs
+++ dotnetcore3.1/s3/{{cookiecutter.project_name}}/test/{{cookiecutter.project_name}}.Tests/FunctionTest.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xunit;
+using Moq;
 using Amazon.Lambda;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
@@ -23,51 +25,52 @@ namespace {{cookiecutter.project_name}}.Tests
         [Fact]
         public async Task TestS3EventLambdaFunction()
         {
-            IAmazonS3 s3Client = new AmazonS3Client(RegionEndpoint.USWest2);
             var context = new TestLambdaContext();
 
             var bucketName = "lambda-S3JsonLogger-".ToLower() + DateTime.Now.Ticks;
             var key = "text.txt";
 
-            // Create a bucket an object to setup a test data.
-            await s3Client.PutBucketAsync(bucketName);
-            try
-            {
-                await s3Client.PutObjectAsync(new PutObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = key,
-                    ContentBody = "sample data"
-                });
+            // BEGIN mocking s3Client
+            var s3ClientMock = new Mock<IAmazonS3>();
+            s3ClientMock.Setup(x => x.Config)
+                        .Returns(new AmazonS3Config { RegionEndpoint = RegionEndpoint.USWest2 });
 
-                // Setup the S3 event object that S3 notifications would create with the fields used by the Lambda function.
-                var s3Event = new S3Event
+            var response = new GetObjectMetadataResponse();
+            response.Headers.ContentType = "text/plain";
+            response.Expires = DateTime.Now;
+
+            s3ClientMock.Setup(x => x.GetObjectMetadataAsync(
+                It.Is<string>(_bucketName => _bucketName == bucketName),    // Mock with the given bucket name
+                It.Is<string>(_key => _key == key),                         // Mock with the given key
+                It.IsAny<CancellationToken>())
+            )
+                .Returns(Task.FromResult(response));
+        
+            var s3Client = s3ClientMock.Object;
+            // END mocking s3Client
+
+
+            // Setup the S3 event object that S3 notifications would create with the fields used by the Lambda function.
+            var s3Event = new S3Event
+            {
+                Records = new List<S3EventNotification.S3EventNotificationRecord>
                 {
-                    Records = new List<S3EventNotification.S3EventNotificationRecord>
+                    new S3EventNotification.S3EventNotificationRecord
                     {
-                        new S3EventNotification.S3EventNotificationRecord
+                        S3 = new S3EventNotification.S3Entity
                         {
-                            S3 = new S3EventNotification.S3Entity
-                            {
-                                Bucket = new S3EventNotification.S3BucketEntity {Name = bucketName },
-                                Object = new S3EventNotification.S3ObjectEntity {Key = key }
-                            }
+                            Bucket = new S3EventNotification.S3BucketEntity { Name = bucketName },
+                            Object = new S3EventNotification.S3ObjectEntity { Key = key }
                         }
                     }
-                };
+                }
+            };
 
-                // Invoke the lambda function and confirm the content type was returned.
-                var function = new Function(s3Client);
-                var contentType = await function.FunctionHandler(s3Event, context);
+            // Invoke the lambda function and confirm the content type was returned.
+            var function = new Function(s3Client);
+            var contentType = await function.FunctionHandler(s3Event, context);
 
-                Assert.Equal("text/plain", contentType);
-
-            }
-            finally
-            {
-                // Clean up the test data
-                await AmazonS3Util.DeleteS3BucketWithObjectsAsync(s3Client, bucketName);
-            }
+            Assert.Equal("text/plain", contentType);
         }
     }
 }
