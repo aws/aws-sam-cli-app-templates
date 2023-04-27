ffb68ca050e729ee6b2f4107a6ba0820a53bb151
Randeep Singh <randeepsingh@Randeeps-MacBook-Air.local>
2023-04-27 05:52:58 -0700

    Adds frontend vue app with all three components

---
+++ nodejs18.x/full-stack/{{cookiecutter.project_name}}/frontend/src/components/CreateItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit.prevent="createItem">
+      <div>
+        <label for="userId">Post User ID</label>
+        <input type="text" id="userId" v-model="formData.userId" />
+      </div>
+      <div>
+        <label for="title">Post Title</label>
+        <input type="text" id="title" v-model="formData.title" />
+      </div>
+      <div>
+        <label for="body">Post Body</label>
+        <input type="text" id="body" v-model="formData.body" />
+      </div>
+      <button>Create Item</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'CreateItem',
+  data() {
+    return {
+      formData: {
+        userId: '',
+        title: '',
+        body: '',
+      },
+    }
+  },
+  methods: {
+    createItem() {
+      axios
+        .post('https://jsonplaceholder.typicode.com/posts', this.formData)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ nodejs18.x/full-stack/{{cookiecutter.project_name}}/frontend/src/components/GetItemById.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>    
+    <form @submit.prevent="createItem">
+      <div>
+        <label for="itemId">Item ID</label>
+        <input type="text" id="itemId" v-model="formData.itemId" />
+      </div>
+      
+      <button @click="getItemsById">Get Item</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'GetItemById',  
+  data() {
+    return {
+      formData: {
+        itemId: '',
+      },      
+      errorMsg: '',
+    }
+  },
+  methods: {
+    getItemsById() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts/' + this.formData.itemId)
+        .then((response) => {
+          console.log(response)
+          this.posts = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+          this.errorMsg = 'Error retrieving data'
+        })
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
