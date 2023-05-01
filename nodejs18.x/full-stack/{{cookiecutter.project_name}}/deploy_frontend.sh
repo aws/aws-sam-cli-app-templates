48aadcc90ed4afb41ea27b93f45489f9c853a215
awsbenpeterson <94929802+awsbenpeterson@users.noreply.github.com>
2023-05-01 20:05:03 +0000

    fixed some bash script problems

--- nodejs18.x/full-stack/{{cookiecutter.project_name}}/deploy_frontend.sh
+++ nodejs18.x/full-stack/{{cookiecutter.project_name}}/deploy_frontend.sh
@@ -7,12 +7,13 @@ read -p "Enter the name of the CloudFormation stack: " stack_name
 api_gateway_endpoint=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='APIGatewayEndpoint'].OutputValue" --output text)
 
 # Get the CloudFront Distribution ID from the stack
-cloudfront_distribution_id=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='CloudfrontDistributionId'].OutputValue" --output text)
+cloudfront_distribution_id=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='CloudFrontDistributionId'].OutputValue" --output text)
 
 # Get the S3 Bucket Name from the stack
 s3_bucket_name=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='WebS3BucketName'].OutputValue" --output text)
 
 # Output the results
-echo "API Gateway URL: $api_gateway_url"
+echo "API Gateway URL: $api_gateway_endpoint"
 echo "CloudFront Distribution ID: $cloudfront_distribution_id"
 echo "S3 Bucket Name: $s3_bucket_name"
+
