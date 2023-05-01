b8177f43ff6c9508270122a274d234f7f465ae13
awsbenpeterson <94929802+awsbenpeterson@users.noreply.github.com>
2023-05-01 19:39:32 +0000

    initial commit

---
+++ nodejs18.x/full-stack/{{cookiecutter.project_name}}/deploy_frontend.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get user input for stack name
+read -p "Enter the name of the CloudFormation stack: " stack_name
+
+# Get the API Gateway URL from the stack
+api_gateway_endpoint=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='APIGatewayEndpoint'].OutputValue" --output text)
+
+# Get the CloudFront Distribution ID from the stack
+cloudfront_distribution_id=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='CloudfrontDistributionId'].OutputValue" --output text)
+
+# Get the S3 Bucket Name from the stack
+s3_bucket_name=$(aws cloudformation describe-stacks --stack-name "$stack_name" --query "Stacks[0].Outputs[?OutputKey=='WebS3BucketName'].OutputValue" --output text)
+
+# Output the results
+echo "API Gateway URL: $api_gateway_url"
+echo "CloudFront Distribution ID: $cloudfront_distribution_id"
+echo "S3 Bucket Name: $s3_bucket_name"
