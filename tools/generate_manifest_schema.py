3b669f83edc7e464afe81af3dde348e9740b2709
Mohamed ElAsmar <melasmar@amazon.com>
2023-05-02 10:33:36 -0700

    add docs in the schema generating the manifest schema file

--- tools/generate_manifest_schema.py
+++ tools/generate_manifest_schema.py
@@ -1,3 +1,8 @@
+# script to generate the manifest file schema.
+# it generates the schema from the current manifest file. It creates a detailed schema to make sure that we can only
+# remove runtimes from the manifest file, but could not add new one, add/remove runtime templates, or change any
+# property values.
+
 import json
 
 schema = {
@@ -16,6 +21,7 @@ with open("../manifest.json") as f:
         for template in manifest[runtime]:
             item = {
                 "type": "object",
+                # this property makes sure that we could add new attributes for the templates
                 "additionalProperties": False,
                 "properties": {
 
@@ -25,12 +31,15 @@ with open("../manifest.json") as f:
             for key in template.keys():
                 item["properties"][key] = {
                     "type": "string",
+                    # this property makes sure that we could not change any attribute value
                     "enum": [
                         template[key]
                     ]
                 }
+                # this property makes sure that we could not remove any of the existing attributes for the templates
                 item["required"].append(key)
             schema["properties"][runtime]["items"].append(item)
+        # these properties make sure that we could not add new templates to any runtime.
         schema["properties"][runtime]["minItems"] = len(schema["properties"][runtime]["items"])
         schema["properties"][runtime]["maxItems"] = len(schema["properties"][runtime]["items"])
     with open("../manifest-schema.json", "w") as ff:
