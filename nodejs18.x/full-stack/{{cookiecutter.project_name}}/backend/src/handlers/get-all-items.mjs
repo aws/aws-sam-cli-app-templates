856817df5f19d2fbfbf52537d9d718127d3fc004
Singh <randeepx@3c063002bfa5.ant.amazon.com>
2023-05-26 23:03:37 -0700

    updates folder structure for lamda handlers and fixes DDB client connection

---
+++ nodejs18.x/full-stack/{{cookiecutter.project_name}}/backend/src/handlers/get-all-items.mjs
@@ -0,0 +1,67 @@
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
+
+//DynamoDB Endpoint
+const ENDPOINT_OVERRIDE = process.env.ENDPOINT_OVERRIDE;
+let ddbClient = undefined;
+
+if(ENDPOINT_OVERRIDE){
+    ddbClient = new DynamoDBClient({ endpoint: ENDPOINT_OVERRIDE });    
+}
+else{
+    
+    ddbClient = new DynamoDBClient({});    // Use default values for DynamoDB endpoint
+    console.warn("No value for ENDPOINT_OVERRIDE provided for DynamoDB, using default");
+}
+
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+
+// Get the DynamoDB table name from environment variables
+const tableName = process.env.SAMPLE_TABLE;
+
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+export const getAllItemsHandler = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    var params = {
+        TableName : tableName
+    };
+
+    try {
+        const data = await ddbDocClient.send(new ScanCommand(params));
+        var items = data.Items;
+    } catch (err) {
+        console.error("Error retrieving all items:", err.message);
+        console.error("Error code:", err.code);
+        console.error("Error name:", err.name);
+        console.error("Error stack:", err.stack);
+        
+    throw err;
+    }
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*", //DO NOT USE THIS VALUE IN PRODUCTION - https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-cors.html
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        body: JSON.stringify(items)
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
