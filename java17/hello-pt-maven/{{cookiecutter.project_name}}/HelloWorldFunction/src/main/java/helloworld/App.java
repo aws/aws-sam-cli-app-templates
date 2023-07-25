ef5f602df626ea02c4917451a8fb218401ce672d
Alexey Soshin <alexey.soshin@gmail.com>
2023-07-25 01:35:11 +0100

    chore: bump PowerTools Java version to 1.16.1 (#438)
    * Bump PowerTools version to 1.16.0
    
    * Bump Gradle version to 7.6.2, latest stable Gradle 7
    
    * Switch to dev.aspectj plugin for Java 11
    
    * Fix what I assume a typo in Cookiecutter template project name
    
    * Java 11 Tests were using 2-space formatting, while the code uses 4-space
    
    * Java 8 Tests were using 2-space formatting, while the code uses 4-space
    
    * Would be useful to apply some Java17 features in the test app, to make sure it actually builds and runs in the correct runtime. Using type inference and text blocks
    
    * Would be useful to apply some Java11 features in the test app, to make sure it actually builds and runs in the correct runtime. Using type inference
    
    * Update java11/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java11/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java17/hello-pt-gradle/{{cookiecutter.project_name}}/HelloWorldFunction/build.gradle
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8.al2/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java11/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java17/hello-pt-gradle/{{cookiecutter.project_name}}/HelloWorldFunction/build.gradle
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8.al2/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java17/hello-pt-gradle/{{cookiecutter.project_name}}/HelloWorldFunction/build.gradle
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java17/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java17/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Update java8.al2/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/pom.xml
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    
    * Bump minor version of Java17 Maven template for applying the latest version
    
    * Add import for AspectJ Gradle plugin
    
    * Lint Java17 test
    
    * Gradle is looking for log4j2.xml by default
    
    https://logging.apache.org/log4j/2.x/faq.html
    
    * Log4J is looking for log4j2.xml by default
    
    https://logging.apache.org/log4j/2.x/faq.html
    
    * Java17 requires AspectJ Tools 1.9.8 or greater
    
    https://github.com/eclipse-aspectj/aspectj/blob/master/docs/dist/doc/JavaVersionCompatibility.md
    
    ---------
    
    Co-authored-by: Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
    Co-authored-by: Daniel Mil <84205762+mildaniel@users.noreply.github.com>

--- java17/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/src/main/java/helloworld/App.java
+++ java17/hello-pt-maven/{{cookiecutter.project_name}}/HelloWorldFunction/src/main/java/helloworld/App.java
@@ -45,7 +45,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     @Metrics(captureColdStart = true)
     {%- endif %}
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        Map<String, String> headers = new HashMap<>();
+        var headers = new HashMap<String, String>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
 
@@ -53,7 +53,8 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
                 .withHeaders(headers);
         try {
             final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", pageContents);
+            String output = String.format("""
+                    { "message": "hello world", "location": "%s" }""", pageContents);
 
             return response
                     .withStatusCode(200)
@@ -71,7 +72,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         {%- if cookiecutter[ "Powertools for AWS Lambda (Java) Logging" ] == "enabled" %}
         log.info("Retrieving {}", address);
         {%- endif %}
-        URL url = new URL(address);
+        var url = new URL(address);
         try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
             return br.lines().collect(Collectors.joining(System.lineSeparator()));
         }
