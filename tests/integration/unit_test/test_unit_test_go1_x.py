54427ad6bd6773f9b0d434a416414aa9d2982178
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2024-02-21 14:51:01 -0800

    Revert "Remove go1.x init templates" (#490)
    * Revert "remove go1.x init templates (#486)"
    
    This reverts commit e3bdf85c16d49b36f981059336114754911e936a.
    
    * Update python3.9/hello-img/README.md
    
    Co-authored-by: Haresh Nasit <84355507+hnnasit@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Haresh Nasit <84355507+hnnasit@users.noreply.github.com>

--- tests/integration/unit_test/test_unit_test_go1_x.py
+++ tests/integration/unit_test/test_unit_test_go1_x.py
@@ -2,6 +2,10 @@ from unittest import skip
 from tests.integration.unit_test.unit_test_base import UnitTestBase
 
 
+class UnitTest_go1_x_cookiecutter_aws_sam_hello_golang(UnitTestBase.GoUnitTestBase):
+    directory = "go1.x/hello"
+    code_directories = ["hello-world"]
+
 class UnitTest_provided_go_cookiecutter_aws_sam_hello_golang(UnitTestBase.GoUnitTestBase):
     directory = "al2/go/hello"
     code_directories = ["hello-world"]
@@ -10,14 +14,27 @@ class UnitTest_providedal2023_go_cookiecutter_aws_sam_hello_golang(UnitTestBase.
     directory = "al2023/go/hello"
     code_directories = ["hello-world"]
 
+class UnitTest_go1_x_cookiecutter_aws_sam_eventbridge_hello_golang(UnitTestBase.GoUnitTestBase):
+    directory = "go1.x/event-bridge"
+    code_directories = ["hello-world"]
+
 class UnitTest_provided_go_cookiecutter_aws_sam_eventbridge_hello_golang(UnitTestBase.GoUnitTestBase):
     directory = "al2/go/event-bridge"
+    code_directories = ["hello-world"]
+
+@skip("eventbridge schema app requires credential to pull missing files, skip")
+class UnitTest_go1_x_cookiecutter_aws_sam_eventbridge_schema_app_golang(UnitTestBase.GoUnitTestBase):
+    directory = "go1.x/event-bridge-schema"
     code_directories = ["hello-world"]
 
 @skip("eventbridge schema app requires credential to pull missing files, skip")
 class UnitTest_provided_go_cookiecutter_aws_sam_eventbridge_schema_app_golang(UnitTestBase.GoUnitTestBase):
     directory = "al2/go/event-bridge-schema"
     code_directories = ["hello-world"]
+
+class UnitTest_go1_x_cookiecutter_aws_sam_hello_step_functions_sample_app(UnitTestBase.GoUnitTestBase):
+    directory = "go1.x/step-func"
+    code_directories = ["functions/stockBuyer", "functions/stockChecker", "functions/stockSeller"]
 
 class UnitTest_provided_go_cookiecutter_aws_sam_hello_step_functions_sample_app(UnitTestBase.GoUnitTestBase):
     directory = "al2/go/step-func"
