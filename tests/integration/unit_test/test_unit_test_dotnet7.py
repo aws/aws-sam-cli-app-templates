dc3e2d63332d1dac1557581c06ed323a0aab1d58
Wing Fung Lau <4760060+hawflau@users.noreply.github.com>
2024-07-29 11:47:17 -0700

    Deprecate aot.dotnet7 template (#508)
    * deprecate aot.dotnet7
    
    * remove test files

--- tests/integration/unit_test/test_unit_test_dotnet7.py
+++
@@ -1,7 +0,0 @@
-# dotnet unit tests are executed from test directories, so code_directories point to test directories
-from tests.integration.unit_test.unit_test_base import UnitTestBase
-
-
-class UnitTest_dotnet6_cookiecutter_aws_sam_hello_dotnet(UnitTestBase.DotNetCoreUnitTestBase):
-    directory = "dotnet7-aot/hello"
-    code_directories = ["test/HelloWorld.Test"]
