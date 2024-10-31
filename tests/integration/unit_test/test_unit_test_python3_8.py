162775d96b5246fd156d1407decee533b92dc9a1
Haresh Nasit <84355507+hnnasit@users.noreply.github.com>
2024-10-31 09:41:21 -0700

    chore: Remove lint tests for python3.8 (#514)
    * chore: Remove linting for python3.8 unit tests
    
    * chore: Remove lint tests for python3.8 from build_invoke

--- tests/integration/unit_test/test_unit_test_python3_8.py
+++ tests/integration/unit_test/test_unit_test_python3_8.py
@@ -4,19 +4,27 @@ from tests.integration.unit_test.unit_test_base import UnitTestBase
 class UnitTest_python3_8_cookiecutter_aws_sam_hello_python(UnitTestBase.Python38UnitTestBase):
     directory = "python3.8/hello"
     code_directories = ["hello_world"]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 class UnitTest_python3_8_cookiecutter_aws_sam_hello_pt_python(UnitTestBase.Python38UnitTestBase):
     directory = "python3.8/hello-pt"
     code_directories = ["hello_world"]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 class UnitTest_python3_8_cookiecutter_aws_sam_eventBridge_python(UnitTestBase.Python38UnitTestBase):
     directory = "python3.8/event-bridge"
     code_directories = ["hello_world_function"]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 
 class UnitTest_python3_8_cookiecutter_aws_sam_eventbridge_schema_app_python(UnitTestBase.Python38UnitTestBase):
     directory = "python3.8/event-bridge-schema"
     code_directories = ["hello_world_function"]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
     def _test_unit_tests(self, code_directory: str):
         self.skipTest("eventbridge schema app requires credential to pull missing files, skip")
@@ -30,11 +38,15 @@ class UnitTest_python3_8_cookiecutter_aws_sam_step_functions_sample_app(UnitTest
         "functions/stock_checker",
         "functions/stock_seller",
     ]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 
 class UnitTest_python3_8_cookiecutter_aws_sam_efs_python(UnitTestBase.Python38UnitTestBase):
     directory = "python3.8/efs"
     code_directories = ["hello_efs"]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 
 class UnitTest_python3_8_cookiecutter_aws_sam_quick_start_web_with_connectors(UnitTestBase.Python38UnitTestBase):
@@ -44,6 +56,8 @@ class UnitTest_python3_8_cookiecutter_aws_sam_quick_start_web_with_connectors(Un
         "src/handlers/get_by_id", 
         "src/handlers/put_item"
     ]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
 
 
 class UnitTest_python3_8_cookiecutter_aws_sam_step_functions_with_connectors(UnitTestBase.Python38UnitTestBase):
@@ -53,3 +67,5 @@ class UnitTest_python3_8_cookiecutter_aws_sam_step_functions_with_connectors(Uni
         "functions/stock_checker",
         "functions/stock_seller",
     ]
+    # python3.8 is deprecated and linter throws an error
+    should_test_lint = False
