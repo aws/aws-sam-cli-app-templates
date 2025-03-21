75b4903a75205abadad59b8e1c4c21ae286b599c
Renato Valenzuela <37676028+valerena@users.noreply.github.com>
2025-03-21 12:03:07 -0700

    feat: add ruby3.4 support (#520)
    * feat: add ruby3.4 support
    
    * minor fixes

--- tests/integration/build_invoke/ruby/test_ruby_3_4.py
+++ tests/integration/build_invoke/ruby/test_ruby_3_4.py
@@ -8,22 +8,22 @@ For each template, it will test the following sam commands:
 3. (if there are event jsons), for each event json, check `sam local invoke` response is a valid json
 """
 
-class BuildInvoke_ruby3_3_cookiecutter_aws_sam_hello_ruby(BuildInvokeBase.HelloWorldExclamationBuildInvokeBase):
-    runtime = "ruby3.3"
+class BuildInvoke_ruby3_4_cookiecutter_aws_sam_hello_ruby(BuildInvokeBase.HelloWorldExclamationBuildInvokeBase):
+    runtime = "ruby3.4"
     directory = "ruby/hello"
 
 
-class BuildInvoke_ruby3_3_cookiecutter_aws_sam_step_functions_sample_app(BuildInvokeBase.BuildInvokeBase):
-    runtime = "ruby3.3"
+class BuildInvoke_ruby3_4_cookiecutter_aws_sam_step_functions_sample_app(BuildInvokeBase.BuildInvokeBase):
+    runtime = "ruby3.4"
     directory = "ruby/step-func"
 
 
-class BuildInvoke_image_ruby3_3_cookiecutter_aws_sam_hello_ruby_lambda_image(
+class BuildInvoke_image_ruby3_4_cookiecutter_aws_sam_hello_ruby_lambda_image(
     BuildInvokeBase.HelloWorldExclamationBuildInvokeBase
 ):
-    runtime = "ruby3.3"
+    runtime = "ruby3.4"
     directory = "ruby/hello-img"
 
-class BuildInvoke_image_ruby3_3_cookiecutter_aws_sam_quick_start_web(BuildInvokeBase.RubyQuickStartWebBuildInvokeBase):
-    runtime = "ruby3.3"
+class BuildInvoke_image_ruby3_4_cookiecutter_aws_sam_quick_start_web(BuildInvokeBase.RubyQuickStartWebBuildInvokeBase):
+    runtime = "ruby3.4"
     directory = "ruby/web"
---
+++ tests/integration/unit_test/test_unit_test_ruby3_4.py
@@ -0,0 +1,32 @@
+from tests.integration.unit_test.unit_test_base import UnitTestBase
+
+
+class UnitTest_ruby3_4_cookiecutter_aws_sam_hello_ruby(UnitTestBase.RubyUnitTestBase):
+    runtime = "ruby3.4"
+    directory = "ruby/hello"
+    code_directories = ["tests/unit/test_handler.rb"]
+    # It can be removed after SAM CLI consumes cfn-lint 1.32.0, which adds support for ruby3.4
+    should_test_lint = False
+
+
+class UnitTest_ruby3_4_cookiecutter_aws_sam_step_functions_sample_app(UnitTestBase.RubyUnitTestBase):
+    runtime = "ruby3.4"
+    directory = "ruby/step-func"
+    should_test_lint = False
+    code_directories = [
+        "tests/unit/test_stock_buyer.rb",
+        "tests/unit/test_stock_checker.rb",
+        "tests/unit/test_stock_seller.rb",
+    ]
+
+class UnitTest_ruby3_4_cookiecutter_quick_start_web(UnitTestBase.RubyUnitTestBase):
+    runtime = "ruby3.4"
+    directory = "ruby/web"
+    should_test_lint = False
+    code_directories = [
+        "test/test_create_item.rb",
+        "test/test_delete_item.rb",
+        "test/test_get_all_items.rb",
+        "test/test_get_item_by_id.rb",
+        "test/test_update_item.rb"
+    ]
